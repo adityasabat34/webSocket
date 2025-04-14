07eceda6f95e5bec905c9c0dc0c69423a3bea455
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-14 21:00:10 +0530

    cors error fixed and server connected properly

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import cors from "cors";
 
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+// app.use(cors(corsOptions));
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: corsOptions,
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -19,6 +28,6 @@ io.on("connection", (socket) => {
 
 const port = 4001;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is listening on ${port}`);
 });
