96b3fc5183464a617a1ab89e4ab11f0f19be49fb
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-14 20:17:20 +0530

    socket server created

--- server/app.js
+++ server/app.js
@@ -1,9 +1,20 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app = express();
 
+const server = createServer(app);
+
+const io = new Server(server);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  console.log("id", socket.id);
 });
 
 const port = 4001;
